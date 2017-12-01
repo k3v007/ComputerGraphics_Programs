3454047492422f5556b20d5494afe33540ac5a18
lazyKEV <viveksinha.301@gmail.com>
2017-12-01 21:02:20 +0530

    Added rotating tetrahedron with Hindi roll no. on it

---
+++ tetrahedron-with-rollno/rollNoTetrahedron.cpp
@@ -0,0 +1,200 @@
+#include <bits/stdc++.h>
+#include <graphics.h>
+#define MAX         10
+#define PI          3.1416
+#define COS(a)      cos(((a) * PI) / (float)180)
+#define SIN(a)      sin(((a) * PI) / (float)180)
+
+using namespace std;
+
+//x y z coordinate
+typedef struct{
+    long long int x;
+    long long int y;
+    long long int z;
+}coord;
+
+
+//Calculating normal vector using cross-product formula
+coord normalVector(coord edge1, coord edge2)
+{
+    coord normal;
+    normal.x = edge1.y * edge2.z - edge1.z * edge2.y;
+    normal.y = edge1.z * edge2.x - edge1.x * edge2.z;
+    normal.z = edge1.x * edge2.y - edge1.y * edge2.x;
+
+    return normal;
+}
+
+//return dot product of two vectors
+long long int dotProduct(coord a, coord b)
+{
+    return (a.x * b.x + a.y * b.y + a.z * b.z);
+}
+
+//setting the vision co-ordinate
+coord V = {700,400,1000};
+
+//calculating whether the given surface is hidden or not using the Back-face detection technique
+bool hidden(coord sfc[MAX], int n)
+{
+    //sfc array contains the surface vertices in order of cross-product
+    //calculating the midpoint of the surface
+    coord M = {0,0,0};
+    for(int i = 0; i < n; ++i) {
+        M.x += sfc[i].x;
+        M.y += sfc[i].y;
+        M.z += sfc[i].z;
+    }
+    M.x /= n;
+    M.y /= n;
+    M.z /= n;
+
+    //edge1 and edge2 for calculating normal vector
+    coord edge1 = {sfc[1].x - sfc[0].x, sfc[1].y - sfc[0].y, sfc[1].z - sfc[0].z};
+    coord edge2 = {sfc[2].x - sfc[1].x, sfc[2].y - sfc[1].y, sfc[2].z - sfc[1].z};;
+    coord N = normalVector(edge1, edge2);
+    coord MV = {V.x - M.x, V.y - M.y, V.z - M.z};
+
+    long long int dp = dotProduct(MV, N);
+
+    if (dp < 0)
+        return false;   //surface is not hidden
+    else
+        return true;    //surface is hidden
+}
+
+int main()
+{
+    initwindow(1400, 900);
+    int xc = getmaxx() / 2, yc = getmaxy() / 2, page = 0;
+    int s = 4, v = 3, crv = 7;
+    int sz = 200;
+
+    //here we have to use redundant vertices to ease the implementation in for loop
+    //though we have only 4 vertices for tetrahedron
+
+    //tetrahedron vertices stored as [x y z 1] in each column
+                              //S1        S2          S3          S4
+    int vertex[4][s*v] = {  {sz,0,0,    sz,0,0,     0,0,0,      0,sz,0},
+                            {0,0,sz,    0,0,0,      sz,0,0,     0,0,sz},
+                            {0,0,0,     0,sz,0,     0,0,sz,     sz,0,0},
+                            {1,1,1,     1,1,1,      1,1,1,      1,1,1}  };
+
+    //coordinates of roll no. are set according to the surfaces of tetrahedron
+    int rollNo[4][4*crv] = {    {0,0,0,0,           0,0,0,0,            0,0,0,0,      0,0,0,0,         60,130,130,30,   50,10,10,100,   50,80,150,100},
+                                {65,105,115,140,    65,105,115,140,     65,35,5,0,    65,35,5,0,       130,20,-20,40,   140,150,50,50,  140,150,50,50},
+                                {70,110,110,110,    70,30,30,30,        70,40,40,70,  70,110,110,70,   0,0,0,0,         50,80,150,100,  50,10,10,100},
+                                {1,1,1,1,           1,1,1,1,            1,1,1,1,      1,1,1,1,         1,1,1,1,         1,1,1,1,        1,1,1,1}           };
+
+    int ang = 0;
+
+    while(1){
+        setcolor(LIGHTBLUE);    settextstyle(10, HORIZ_DIR, 1);
+        outtextxy(xc - 300, yc - 350, "Press  'a'  for \"CW Rotation\" and  'd'  for \"CCW Rotation\"");
+
+        //x-rotation matrix
+        float rotx[4][4] = {    {1, 0, 0, 0},
+                                {0, COS(ang), -SIN(ang), 0},
+                                {0, SIN(ang), COS(ang), 0},
+                                {0, 0, 0, 1}    };
+
+        //y-rotation matrix
+        float roty[4][4] = {    {COS(ang), 0, SIN(ang), 0},
+                                {0, 1, 0, 0},
+                                {-SIN(ang), 0, COS(ang), 0},
+                                {0, 0, 0, 1}    };
+
+        //z-rotation matrix
+        float rotz[4][4] = {    {COS(ang), -SIN(ang), 0, 0},
+                                {SIN(ang), COS(ang), 0, 0},
+                                {0, 0, 1, 0},
+                                {0, 0, 0, 1}    };
+
+
+        char ch = getch();      ch = tolower(ch);
+        switch(ch){
+            case 'a' :  --ang;  break;
+            case 'd' :  ++ang;  break;
+        }
+
+
+        //multiplying the vertex with the y-rotation matrix
+        float pts[4][s*v];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < s * v; j++) {
+                int sum = 0;
+                for (int k = 0; k < 4; k++)
+                    sum += roty[i][k] * vertex[k][j];
+                pts[i][j] = sum;
+            }
+        }
+
+        //multiplying the roll no. with the y-rotation matrix
+        float rollpts[4][4*crv];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4 * crv; j++) {
+                int sum = 0;
+                for (int k = 0; k < 4; k++) {
+                    sum += 0.8 * roty[i][k] * rollNo[k][j];
+                }
+                rollpts[i][j] = sum;
+            }
+        }
+
+        for (int i = 0; i < s * v; i += v){
+            //converting the vertex matrix into array of co-ordinates of a given surface
+            coord sfc[3] = {{xc + pts[0][i], yc - pts[1][i], pts[2][i]}, {xc + pts[0][i+1], yc - pts[1][i+1], pts[2][i+1]}, {xc + pts[0][i+2], yc - pts[1][i+2], pts[2][i+2]}};
+            //if surface is not hidden then display it
+            if (!hidden(sfc, 3)){
+                int lcolor;
+
+                switch(i){
+                    case 0 :  lcolor = LIGHTMAGENTA;    break;
+                    case 3 :  lcolor = LIGHTGREEN;      break;
+                    case 6 :  lcolor = LIGHTCYAN;       break;
+                    case 9 :  lcolor = YELLOW;          break;
+                }
+
+                setcolor(lcolor);
+                for(int j = 0; j < v; ++j)
+                    line(sfc[j].x, sfc[j].y, sfc[(j+1)%v].x, sfc[(j+1)%v].y);
+
+                if (i == 6) {       //4 is present on S3
+                    for (int col = 0; col < 4 * 4; col +=4) {   //for hindi 4
+                        for (float t = 0; t <= 1; t += 0.001) {
+                            float x, y;
+                            x = pow(1-t, 3) * rollpts[0][col] + 3 * pow(1-t, 2) * t * rollpts[0][col+1] + 3 * (1-t) * t * t * rollpts[0][col+2] + pow(t, 3) * rollpts[0][col+3];
+                            y = pow(1-t, 3) * rollpts[1][col] + 3 * pow(1-t, 2) * t * rollpts[1][col+1] + 3 * (1-t) * t * t * rollpts[1][col+2] + pow(t, 3) * rollpts[1][col+3];
+                            putpixel(xc + x, yc - y, RED);
+                        }
+                    }
+                }
+                else if (i == 0) {          //8 is present on S1
+                    int col = 4*4;          //for hindi 8
+                    for (float t = 0; t <= 1; t += 0.001) {
+                        float x, y;
+                        x = pow(1-t, 3) * rollpts[0][col] + 3 * pow(1-t, 2) * t * rollpts[0][col+1] + 3 * (1-t) * t * t * rollpts[0][col+2] + pow(t, 3) * rollpts[0][col+3];
+                        y = pow(1-t, 3) * rollpts[1][col] + 3 * pow(1-t, 2) * t * rollpts[1][col+1] + 3 * (1-t) * t * t * rollpts[1][col+2] + pow(t, 3) * rollpts[1][col+3];
+                        putpixel(xc + x, yc - y, RED);
+                    }
+                }
+                else if (i == 9) {      //0 is present on S4
+                    for (int col = 4*5; col < 4 * crv; col +=4) {       //for hindi 0
+                        for (float t = 0; t <= 1; t += 0.001) {
+                            float x, y;
+                            x = pow(1-t, 3) * rollpts[0][col] + 3 * pow(1-t, 2) * t * rollpts[0][col+1] + 3 * (1-t) * t * t * rollpts[0][col+2] + pow(t, 3) * rollpts[0][col+3];
+                            y = pow(1-t, 3) * rollpts[1][col] + 3 * pow(1-t, 2) * t * rollpts[1][col+1] + 3 * (1-t) * t * t * rollpts[1][col+2] + pow(t, 3) * rollpts[1][col+3];
+                            putpixel(xc + x, yc - y, RED);
+                        }
+                    }
+                }
+            }
+        }
+
+        setactivepage(page);        setvisualpage(1-page);     cleardevice();
+    }
+
+    getch();
+    return 0;
+}
